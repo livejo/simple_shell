b4b7e9451bdc666f3718e1e4c5975b1b379108b3
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-04-12 16:40:54 +0300

    brief description

--- history.h
+++ history.h
@@ -1,16 +1,18 @@
 #ifndef HISTORY_H
 #define HISTORY_H
+
 /**
  * struct HistList - singly linked list
- * @cmd: command
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * @cmd: command line entered
+ * @next: pointer to the next node
+ * Description: singly linked list node structure for History 
  * for Holberton project
  */
+
 typedef struct HistList
 {
 	char *cmd;
 	struct HistList *next;
 } HistList;
+
 #endif
